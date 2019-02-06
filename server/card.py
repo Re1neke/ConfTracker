aa7519accf152b9c3467991df6ac7f392dbcd027
Max Reineke <reinekekun@gmail.com>
2019-02-06 14:36:34 +0200

    Docstring to card.py is added

--- server/card.py
+++ server/card.py
@@ -1,15 +1,32 @@
-#/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""This modude contains class for interacting with RFID tag reader and some
+helper functions for it.
+"""
 
 from pirc522 import RFID
 
-BLKSIZE = 16
+BLKSIZE = 16        # Size of one block in bytes
 
-SECT_ST = 4
-AID_BLOCK = 4
-NAME_BLOCK = 5
-SURNAME_BLOCK = 6
+# Default key for getting access to card section
+DEF_KEY = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
+
+SECT_ST = 4         # Number of first block of the used section
+AID_BLOCK = 4       # Number of block where attendee's id is stored
+NAME_BLOCK = 5      # And block for attendee's name
+SURNAME_BLOCK = 6   # And one for surname
+
 
 def strtoblk(string):
+    """Converts string to bytearray of block size lenth
+    and zeroes rest of block.
+
+    Args:
+        string (str): string that will be converted.
+
+    Returns:
+        list: list with raw byte values.
+
+    """
     blk = list(bytes(string.encode('ascii')))
     while len(blk) < BLKSIZE:
         blk += [0]
@@ -17,17 +34,34 @@ def strtoblk(string):
 
 
 def inttoblk(num):
-    return (num).to_bytes(BLKSIZE, 'little') 
+    """Converts number to bytearray block.
+
+    Args:
+        num (int): number to convert.
+
+    Returns:
+        bytes: bytearray with little-endian int value.
+
+    """
+    return (num).to_bytes(BLKSIZE, 'little')
 
 
 class CardHandler(RFID):
+    """Class for writing and reading attendee's data with RFID tags.
+    """
 
     def __del__(self):
         self.cleanup()
 
     def _interact_card(self):
+        """Prepares card for interacting.
+
+        Returns:
+            bool: True if card reary for interaction. False otherwise.
+
+        """
         self.wait_for_tag()
-        error, tag_type = self.request()
+        error, _ = self.request()
         if error:
             return False
         error, uid = self.anticoll()
@@ -35,10 +69,19 @@ class CardHandler(RFID):
             return False
         if self.select_tag(uid):
             return False
-        if not self.card_auth(self.auth_a, SECT_ST, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF], uid):
-            return True
+        if self.card_auth(self.auth_a, SECT_ST, DEF_KEY, uid):
+            return False
+        return True
 
     def read_card(self):
+        """Reads data from card and returs tuple of attendee's id, name and
+        surname.
+
+        Returns:
+            tuple: tuple with id, name and surname of attendee or None if some
+                error occurs.
+
+        """
         if not self._interact_card():
             return None
         error, aid_list = self.read(AID_BLOCK)
@@ -56,13 +99,24 @@ class CardHandler(RFID):
         self.stop_crypto()
         return aid, sname, fname
 
-    def write_card(self, aid, sname, fname):
+    def write_card(self, aid, name, sname):
+        """Writes attendee's data to card.
+
+        Args:
+            aid (int): id of attendee.
+            name (str): attendee's name.
+            sname (str): attendee's surname.
+
+        Returns:
+            bool: returns True if data is written successfuly, False otherwise.
+
+        """
         if not self._interact_card():
             return False
         if self.write(AID_BLOCK, inttoblk(aid)):
             return False
-        if self.write(NAME_BLOCK, strtoblk(sname)):
+        if self.write(NAME_BLOCK, strtoblk(name)):
             return False
-        if self.write(SURNAME_BLOCK, strtoblk(fname)):
+        if self.write(SURNAME_BLOCK, strtoblk(sname)):
             return False
         return True
